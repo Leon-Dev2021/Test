9992258d466051866a385b58345c4e99ae771d2e
Leon-Dev2021 <91741541+Leon-Dev2021@users.noreply.github.com>
2021-11-17 08:21:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler
+from os import environ as env
 
 
 def start(ud: Update, ctx: CallbackContext):
     ud.message.reply_text('Hola')
     
 if __name__ == '__main__':
-    updater = Updater('1810373040:AAHBEoOG1h4FxZ4QCzRfAIo9g9P24DF3F2w')
+    updater = Updater(env['TOKEN'])
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start',start))
     updater.start_polling()
